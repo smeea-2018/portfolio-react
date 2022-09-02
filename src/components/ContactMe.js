aa44ad7f40b00b49126011b2b2a2c24cf0b0d692
smeea-2018 <smeeaarshad@gmail.com>
2022-09-02 16:04:16 +0100

    contact ME created

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,104 +1,80 @@
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
 import Paper from "@mui/material/Paper";
 import Divider from "@mui/material/Divider";
-import "../App.css";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import { useMediaQuery } from "@mui/material";
-import { useForm } from "react-hook-form";
-// import { Form } from "react-bootstrap";
+import Link from "@mui/material/Link";
 
-export default function Message() {
-  const isMobile = useMediaQuery("(max-width:900px)");
+import { Button } from "@mui/material";
 
+export default function Message({ isMobile }) {
   const {
     register,
-    handleSubmit,
     formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => {
-    console.log("done");
-    console.log(data);
+    handleSubmit,
+  } = useForm({
+    mode: "onBlur",
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onSubmit = (formData) => {
+    console.log(formData);
   };
 
   return (
-    <div>
-      <div id="contact-me">
-        <Paper
-          sx={{
-            "& .MuiTextField-root": {
-              m: 1,
-              minWidth: isMobile ? "100%" : "40ch",
-            },
-            display: "flex",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "space-between",
-            alignItems: "center",
-            p: 3,
-            m: 8,
-          }}
-        >
-          <Stack
-            component="form"
-            sx={{
-              p: 5,
-              m: 8,
-              display: "flex",
-              flexDirection: "column",
-              flexWrap: "wrap",
-              justifyContent: "space-between",
-            }}
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <Stack spacing={2}>
-              <Typography component="h1" variant="h4" align="center">
-                Contact Me
-              </Typography>
-
-              <Divider />
-
-              <TextField
-                required
-                error={!!errors.name}
-                label="Name"
-                variant="outlined"
-                {...register("name", {
-                  required: true,
-                })}
-                helperText={
-                  !!errors.name ? "Please enter your first name." : ""
-                }
-              />
-
-              <TextField
-                required
-                label="Email"
-                variant="outlined"
-                error
-                helperText="Please enter your email address."
-              />
-
-              <TextField
-                required
-                label="Message"
-                variant="outlined"
-                error
-                helperText="Please enter message"
-              />
-              <Button
-                variant="contained"
-                type="submit"
-                sx={{ backgroundColor: "blue" }}
-              >
-                Send
-              </Button>
-            </Stack>
-          </Stack>
-        </Paper>
-      </div>
-    </div>
+    <Paper sx={{ p: 3, minWidth: isMobile ? "90%" : "400px" }} elevation={6}>
+      <Typography component="h1" variant="h4" align="center">
+        Login
+      </Typography>
+      <Divider />
+      <Stack
+        component="form"
+        sx={{ p: 3 }}
+        spacing={4}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Stack spacing={2}>
+          <TextField
+            error={!!errors.email}
+            label="Name"
+            variant="outlined"
+            helperText={!!errors.email ? "Please enter a valid name." : ""}
+            {...register("mame", {
+              required: true,
+            })}
+          />
+        </Stack>
+        <Stack spacing={2}>
+          <TextField
+            error={!!errors.email}
+            label="Email"
+            variant="outlined"
+            helperText={!!errors.email ? "Please enter a valid email." : ""}
+            {...register("email", {
+              required: true,
+            })}
+          />
+        </Stack>
+        <Stack spacing={2}>
+          <TextField
+            error={!!errors.email}
+            label="Message"
+            variant="outlined"
+            helperText={!!errors.email ? "Please enter a valid message." : ""}
+            {...register("message", {
+              required: true,
+            })}
+          />
+        </Stack>
+        <Stack spacing={2}>
+          <Button variant="contained" type="submit">
+            Send
+          </Button>
+        </Stack>
+      </Stack>
+    </Paper>
   );
 }
